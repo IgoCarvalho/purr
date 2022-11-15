88f6a0eb78732011c9f8d25268968bb50ca76b25
Igo Carvalho <igocarvalho00@gmail.com>
2022-11-15 13:14:08 -0300

    :tada: (web) initial setup

---
+++ web/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import IconPurr from './components/icons/IconPurr.vue';
+</script>
+
+<template>
+  <main class="flex flex-col justify-center items-center gap-4 h-screen">
+    <IconPurr class="text-violet-400" />
+
+    <h1 class="text-3xl font-bold text-white text-center">Hello world!</h1>
+  </main>
+</template>
+
+<style scoped></style>
