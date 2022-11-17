af12e30d317a7c5dda391661e2924990d4639e51
Igo Carvalho <igocarvalho00@gmail.com>
2022-11-17 13:34:42 -0300

    :sparkles: (web) create loading component

---
+++ web/src/components/MyLoading.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Autorenew from 'vue-material-design-icons/Autorenew.vue';
+</script>
+
+<template>
+  <div
+    class="px-4 py-3 w-fit flex mx-auto gap-2 rounded bg-purr-pink/80 border border-purr-pink"
+  >
+    <Autorenew class="animate-spin" />
+    <p>Carregando...</p>
+  </div>
+</template>
