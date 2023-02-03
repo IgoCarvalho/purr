30f8d6eb3217b99b15c247636e31b0399983d861
Igo Carvalho <igocarvalho00@gmail.com>
2023-02-03 11:29:08 -0300

    :lipstick: (web) add icons to MyInput component

--- web/src/components/MyInput.vue
+++ web/src/components/MyInput.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+import type { Component } from 'vue';
+
 interface InputProps {
   modelValue: string;
   type: string;
   placeholder?: string;
   name: string;
   label?: string;
+  leftIcon?: Component;
+  rightIcon?: Component;
 }
 defineProps<InputProps>();
 
@@ -21,14 +25,25 @@ function updateValeu(e: Event) {
 <template>
   <div>
     <label v-if="label" :for="name">{{ label }}</label>
-    <input
-      :id="name"
-      :type="type"
-      :name="name"
-      :value="modelValue"
-      :placeholder="placeholder"
-      class="bg-gray-800 border border-gray-700 px-3 py-2 rounded-md block w-full hover:border-gray-600 focus:border-purr-pink focus:outline-purr-pink/30 focus:outline"
-      @input="updateValeu"
-    />
+    <div
+      :class="`
+        flex gap-2 w-full px-3 py-2 rounded
+      bg-gray-800 border border-gray-700 hover:border-gray-600 text-gray-400 focus-within:text-purr-pink
+        focus-within:outline-1 focus-within:outline focus-within:outline-purr-pink focus-within:-outline-offset-1
+        focus-within:ring-2 focus-within:ring-purr-pink/30
+      `"
+    >
+      <component class="min-w-[24px] h-6" :is="leftIcon" v-if="leftIcon" />
+      <input
+        :id="name"
+        :type="type"
+        :name="name"
+        :value="modelValue"
+        :placeholder="placeholder"
+        class="bg-transparent outline-none flex-1 text-gray-100"
+        @input="updateValeu"
+      />
+      <component class="min-w-[24px] h-6" :is="rightIcon" v-if="rightIcon" />
+    </div>
   </div>
 </template>
